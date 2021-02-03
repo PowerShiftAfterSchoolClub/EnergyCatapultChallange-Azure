21c19e38e8ee090836a87ce3c1fe22ec68597564
Jon Gill <jongillster@gmail.com>
2021-02-03 18:21:26 +0000

    Initial commit

---
+++ Energy.Catapult.Challenge/Energy.Catapult.Challenge.Azure.Functions/Functions/PvForecast.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Energy.Catapult.Challenge.Azure.Functions.Functions
+{
+    public static class PvForecast
+    {
+        [FunctionName("PvForecast")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("PV Forecast");
+        }
+    }
+}
