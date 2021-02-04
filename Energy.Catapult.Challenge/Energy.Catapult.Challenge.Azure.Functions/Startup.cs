8c970a24ca2829b21b6b47ff558cbcc791baf1d3
Jon Gill <jongillster@gmail.com>
2021-02-04 18:50:09 +0000

    Got DI basics in place

---
+++ Energy.Catapult.Challenge/Energy.Catapult.Challenge.Azure.Functions/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+[assembly: FunctionsStartup(typeof(Energy.Catapult.Challenge.Azure.Functions.Startup))]
+
+
+namespace Energy.Catapult.Challenge.Azure.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+        }
+    }
+}
