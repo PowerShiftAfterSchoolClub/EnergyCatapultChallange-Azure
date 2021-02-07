1a0a138e0935b1b163864bebc35a647acc13d3af
Jon Gill <jongillster@gmail.com>
2021-02-07 13:19:30 +0000

    Added simple swagger

--- Energy.Catapult.Challenge/Energy.Catapult.Challenge.Azure.Functions/Functions/PvForecast.cs
+++ Energy.Catapult.Challenge/Energy.Catapult.Challenge.Azure.Functions/Functions/PvForecast.cs
@@ -11,6 +11,7 @@ using System.Text.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace Energy.Catapult.Challenge.Azure.Functions.Functions
 {
@@ -23,6 +24,80 @@ namespace Energy.Catapult.Challenge.Azure.Functions.Functions
             this.forecaster = forecaster ?? throw new System.ArgumentNullException(nameof(forecaster));
         }
 
+        /// <summary>
+        /// Generates a forecast from ML model and saves to datastore.
+        /// </summary>
+        /// <remarks>
+        /// Sample Request
+        /// 
+        /// POST /PvForecast
+        /// {
+        ///   "data": [
+        ///     {
+        ///       "dateTimeUTC": "2018-07-22 12:00:00.0000000",
+        ///       "temp_location3": 19.7,
+        ///       "temp_location6": 23.056,
+        ///       "temp_location2": 24.73,
+        ///       "temp_location4": 19.88,
+        ///       "temp_location5": 22.14,
+        ///       "temp_location1": 22.14,
+        ///       "solar_location3": 831,
+        ///       "solar_location6": 865,
+        ///       "solar_location2": 861,
+        ///       "solar_location4": 832,
+        ///       "solar_location5": 863,
+        ///       "solar_location1": 858,
+        ///       "summerWinter": "SUMMER",
+        ///       "dateTimeLocal": "2018-07-22 12:00:00.0000000",
+        ///       "year": 2018,
+        ///       "monthNum": 7,
+        ///       "monthName": "Jul",
+        ///       "weekNumber": 30,
+        ///       "dayOfWeek": "Sun",
+        ///       "dayOfWeekNumber": 1,
+        ///       "hourText": 12,
+        ///       "hourNumber": 12,
+        ///       "settlementPeriod": 25,
+        ///       "bankHoliday": "",
+        ///       "workingDay": ""
+        ///     },
+        ///      {
+        ///       "dateTimeUTC": "2018-07-22 12:30:00.0000000",
+        ///       "temp_location3": 19.7,
+        ///       "temp_location6": 23.056,
+        ///       "temp_location2": 24.73,
+        ///       "temp_location4": 19.88,
+        ///       "temp_location5": 22.14,
+        ///       "temp_location1": 22.14,
+        ///       "solar_location3": 22,
+        ///       "solar_location6": 865,
+        ///       "solar_location2": 861,
+        ///       "solar_location4": 832,
+        ///       "solar_location5": 863,
+        ///       "solar_location1": 858,
+        ///       "summerWinter": "SUMMER",
+        ///       "dateTimeLocal": "2018-07-22 12:30:00.0000000",
+        ///       "year": 2018,
+        ///       "monthNum": 7,
+        ///       "monthName": "Jul",
+        ///       "weekNumber": 30,
+        ///       "dayOfWeek": "Sun",
+        ///       "dayOfWeekNumber": 1,
+        ///       "hourText": 12,
+        ///       "hourNumber": 12,
+        ///       "settlementPeriod": 25,
+        ///       "bankHoliday": "",
+        ///       "workingDay": ""
+        ///     }
+        ///   ]
+        /// }
+        /// 
+        /// </remarks>
+        /// <param name="req">Forecast Request</param>
+        /// <param name="log">Logger</param>
+        /// <returns>Generated forecast</returns>
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(List<ForecastPeriod>))]
+        [Produces("application/json")]
         [FunctionName("PvForecast")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "POST", Route = null)] HttpRequest req,
--- Energy.Catapult.Challenge/Energy.Catapult.Challenge.Azure.Functions/Startup.cs
+++ Energy.Catapult.Challenge/Energy.Catapult.Challenge.Azure.Functions/Startup.cs
@@ -2,6 +2,8 @@
 using Energy.Catapult.Challenge.Azure.Functions.Domain.Model;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using AzureFunctions.Extensions.Swashbuckle;
 
 
 [assembly: FunctionsStartup(typeof(Energy.Catapult.Challenge.Azure.Functions.Startup))]
@@ -16,6 +18,9 @@ namespace Energy.Catapult.Challenge.Azure.Functions
             builder.Services.AddHttpClient();
             //builder.Services.AddSingleton<IServiceCollection>(builder.Services);
             builder.Services.AddTransient<IForecaster<PvForecastRequest, ForecastResult>, PvForecaster>();
+
+            // Add swagger support
+            builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
         }
     }
 }
